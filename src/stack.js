8349cfd5780a591193551687258526bfb749193c
FangBinwei <fangbinwei@qq.com>
2018-04-18 21:47:39 +0800

    zepto对集合操作之后继续保存上一个集合的状态

--- src/stack.js
+++ src/stack.js
@@ -4,10 +4,12 @@
 
 ;(function($){
   $.fn.end = function(){
+    // 返回上一个状态
     return this.prevObject || $()
   }
 
   $.fn.andSelf = function(){
+    // 将上一个状态的集合加入到现集合中
     return this.add(this.prevObject || $())
   }
 
@@ -15,6 +17,7 @@
     var fn = $.fn[property]
     $.fn[property] = function(){
       var ret = fn.apply(this, arguments)
+      // 保存这些操作之前 原来的集合
       ret.prevObject = this
       return ret
     }
