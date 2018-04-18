cb4489787fcef6587faf602df1838b596363a138
FangBinwei <fangbinwei@qq.com>
2018-04-18 20:41:17 +0800

    ie 模块, 改写getComputedStyle()

--- src/ie.js
+++ src/ie.js
@@ -5,6 +5,7 @@
 ;(function(){
   // getComputedStyle shouldn't freak out when called
   // without a valid element as argument
+  // 有些浏览器getComputedStyle()第一个参数不是正常的element时,会报错, 如果有这种情况return null,防止报错
   try {
     getComputedStyle(undefined)
   } catch(e) {
